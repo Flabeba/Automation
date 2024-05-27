57e0dbabe4ad6d134f15c673eb1aa01a1a6387db
Flabeba <pantherlily6@gmail.com>
2024-05-27 09:34:20 +0300

    Домашняя работа 7

--- Lession_7/test_calculate.py
+++ Lession_7/test_calculate.py
@@ -5,7 +5,6 @@ from classes.calculate import Calculate
 
 var_value = ["7", "+", "8", "=", "45", "15"]
 
-<<<<<<< HEAD
 def test_calculate(var_value):
 
     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
@@ -17,12 +16,3 @@ def test_calculate(var_value):
     waiting_calc.close(driver)
 
 test_calculate(var_value)
-=======
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-waiting_calc = Calculate(driver)
-waiting_calc.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-waiting_calc.waiting_time(driver, var_value)
-waiting_calc.calc(driver, var_value)
-waiting_calc.waiting(driver, var_value)
-waiting_calc.close(driver)
->>>>>>> refs/remotes/origin/lesson7
